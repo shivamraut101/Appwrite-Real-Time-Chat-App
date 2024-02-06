4976f0baf12cfda806d1a9e1e458cd324d92ed38
shivamraut101 <shivamkumarraut12@gmail.com>
2024-02-06 22:43:30 +0530

    AuthProvider, PrivateRoute,LoginPage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import { Room } from './pages/Room'
-import './App.css'
+import { useState } from "react";
+import { Room } from "./pages/Room";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { AuthProvider } from "./utils/AuthContext";
 
 function App() {
-
   return (
     <>
-      <Room />
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/" element={<Room />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

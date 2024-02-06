ca3ec7e48f01afcd076216413ff26915c3fa6d23
shivamraut101 <shivamkumarraut12@gmail.com>
2024-02-06 22:44:41 +0530

    LoginPage Layout

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useAuth } from '../utils/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
 
 export const LoginPage = () => {
+  const {user} = useAuth()
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if(user){
+      navigate('/')
+    }
+  },[])
   return (
     <div>LoginPage</div>
   )

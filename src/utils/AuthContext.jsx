b12e53a515afeb4b323d5f5fdda36112f12a4a2a
shivamraut101 <shivamkumarraut12@gmail.com>
2024-02-11 19:50:14 +0530

    Login Part Done

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,25 +1,40 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { account } from "../appwriteConfig";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
 
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(false);
+  const navigate = useNavigate()
+
+  const handleInputLogin = async (e,email,password)=>{
+    e.preventDefault()
+    try {
+      const response = await account.createEmailSession(email, password);
+      console.log("LogIn Done", response)
+      const accountdDetails = account.get()
+      setUser(accountdDetails)
+      navigate("/")
+    }catch (error) {
+      console.error(error);
+    }
+  }
 
   const contextData = {
-      user
+      user,
+      handleInputLogin
   };
 
   useEffect(()=>{
     setLoading(false); 
   },[])
-
-  return (
-    <AuthContext.Provider value={contextData}>
-      {loading ? <p>Loading...</p> : children}
-    </AuthContext.Provider>
-  );
+  
+  return <AuthContext.Provider value={contextData}>
+          {loading ? <p>Loading...</p>: children}
+        </AuthContext.Provider>
 };
 
 export const useAuth = () =>{

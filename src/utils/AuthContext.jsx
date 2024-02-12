4f97541df250c4d60510a0cdca3197f6957a612f
shivamraut101 <shivamkumarraut12@gmail.com>
2024-02-12 20:57:59 +0530

    AuthContext Done

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,44 +1,81 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { account } from "../appwriteConfig";
 import { useNavigate } from "react-router-dom";
+import { ID } from "appwrite";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-
   const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState(false);
-  const navigate = useNavigate()
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-  const handleInputLogin = async (e,email,password)=>{
-    e.preventDefault()
+  const handleInputLogin = async (e, email, password) => {
+    e.preventDefault();
     try {
       const response = await account.createEmailSession(email, password);
-      console.log("LogIn Done", response)
-      const accountdDetails = account.get()
-      setUser(accountdDetails)
-      navigate("/")
-    }catch (error) {
+      console.log("LogIn Done", response);
+      const accountdDetails = await account.get();
+      console.log("from handlelogin", accountdDetails);
+      setUser(accountdDetails);
+      navigate("/");
+    } catch (error) {
       console.error(error);
     }
-  }
-
-  const contextData = {
-      user,
-      handleInputLogin
   };
 
-  useEffect(()=>{
-    setLoading(false); 
-  },[])
-  
-  return <AuthContext.Provider value={contextData}>
-          {loading ? <p>Loading...</p>: children}
-        </AuthContext.Provider>
+  const handleUserLogOut = async () => {
+    await account.deleteSession("current");
+    setUser(null);
+  };
+
+  const handleUserRegister = async (e, email, password, name) => {
+    e.preventDefault();
+    try {
+      let response = await account.create(ID.unique(), email, password, name);
+      console.log("registered", response);
+      await account.createEmailSession(email,password)
+      const accountdDetails = await account.get();
+      console.log("From UserRegistered", accountdDetails);
+      setUser(accountdDetails);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const contextData = {
+    user,
+    handleInputLogin,
+    handleUserLogOut,
+    handleUserRegister,
+  };
+
+  useEffect(() => {
+    getUserOnLoad();
+  }, []);
+
+  const getUserOnLoad = async () => {
+    try {
+      const accountdDetails = await account.get();
+      console.log("UserOnload", accountdDetails);
+      setUser(accountdDetails);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? <p>Loading...</p> : children}
+    </AuthContext.Provider>
+  );
 };
 
-export const useAuth = () =>{
-  return useContext(AuthContext)
-}
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
-export default AuthContext
+export default AuthContext;
